a4df770abfe107e1b5b1416f8539436f47ff8488
M.Jafaryan <m.jafaryan2006@gmail.com>
2025-12-06 17:48:32 +0330

    feat: customize the admin panel for app models

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserModel
 
-# Register your models here.
+
+class UserModelAdmin(UserAdmin):
+    """Admin customization for UserModel(custom user) model"""
+
+    model = UserModel
+
+    list_display = ("username", "email", "is_superuser",)
+    list_filter = ("is_superuser",)
+    search_fields = ("username", "email",)
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {
+                "fields": ("username", "email", "password",)
+            }),
+        ("Permissions", {
+                "fields": ("is_active", "is_staff", "is_superuser",)
+            }),
+        ("Group permissions", {
+                "fields": ("groups", "user_permissions",)
+        }),
+        ("Important dates", {
+                "fields": ("last_login", "date_joined",)
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+                "classes": ("wide",),
+                "fields": (
+                    "username", "email", "password1", "password2",
+                    "is_active", "is_staff", "is_superuser",
+                    )
+            }),
+    )
+
+
+admin.site.register(UserModel, UserModelAdmin)
--- core/tasks/admin.py
+++ core/tasks/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import TaskModel
 
-# Register your models here.
+
+class TaskModelAdmin(admin.ModelAdmin):
+    """Admin customization for TaskModel"""
+
+    model = TaskModel
+
+    list_display = ("title", "user", "created_at", "completed",)
+    list_filter = ("user", "completed",)
+    search_fields = ("user__username", "title",)
+    ordering = ("-created_at",)
+
+    fieldsets = (
+        (None, {
+                "fields": ("title", "completed", "description",),
+        }),
+        ("Important dates", {
+                "fields": ("created_at", "updated_at", "due_date",),
+        }),
+        ("User information", {
+                "fields": ("user",),
+        })
+    )
+    readonly_fields = ("created_at", "updated_at",)
+
+
+admin.site.register(TaskModel, TaskModelAdmin)
