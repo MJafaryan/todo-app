2544875c98047627f4a4efa0c3b0884b66d5d599
M.Jafaryan <m.jafaryan2006@gmail.com>
2025-12-06 13:43:37 +0330

    feat: define user model and add it to settings file

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class UserModel(AbstractUser):
+    """The custom user model of project users."""
+    pass
